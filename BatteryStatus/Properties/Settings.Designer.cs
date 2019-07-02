b59f52b616ba1bebca77ca14462ac07d8a1d5b67
Daniel <danielrinconr@usantotomas.edu.co>
2019-07-02 17:20:25 -0500

    -Edit initial settings

--- BatteryStatus/Properties/Settings.Designer.cs
+++ BatteryStatus/Properties/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace BatteryStatus.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
         public uint PcIdleTime {
             get {
                 return ((uint)(this["PcIdleTime"]));
