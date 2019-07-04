73efa1079fbcfd3a81c8cf5ffc9e990d21e2d122
Daniel <danielrinconr@usantotomas.edu.co>
2019-07-03 22:08:44 -0500

    Updater test

--- BatteryStatus/Forms/FormAbout.Designer.cs
+++ BatteryStatus/Forms/FormAbout.Designer.cs
@@ -35,6 +35,8 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.LbVersionTxt = new System.Windows.Forms.Label();
             this.LbVersion = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,16 +46,16 @@
             this.lb_InfoAuthor.Location = new System.Drawing.Point(178, 25);
             this.lb_InfoAuthor.Name = "lb_InfoAuthor";
             this.lb_InfoAuthor.Size = new System.Drawing.Size(175, 17);
-            this.lb_InfoAuthor.TabIndex = 6;
+            this.lb_InfoAuthor.TabIndex = 0;
             this.lb_InfoAuthor.Text = "Creado por: Daniel Rincón";
             // 
             // lnklb_emailAuthor
             // 
             this.lnklb_emailAuthor.AutoSize = true;
-            this.lnklb_emailAuthor.Location = new System.Drawing.Point(178, 51);
+            this.lnklb_emailAuthor.Location = new System.Drawing.Point(178, 45);
             this.lnklb_emailAuthor.Name = "lnklb_emailAuthor";
             this.lnklb_emailAuthor.Size = new System.Drawing.Size(232, 17);
-            this.lnklb_emailAuthor.TabIndex = 7;
+            this.lnklb_emailAuthor.TabIndex = 1;
             this.lnklb_emailAuthor.TabStop = true;
             this.lnklb_emailAuthor.Text = "danielrinconr@usantotomas.edu.co";
             // 
@@ -62,7 +64,7 @@
             this.label1.Location = new System.Drawing.Point(16, 150);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(578, 51);
-            this.label1.TabIndex = 8;
+            this.label1.TabIndex = 6;
             this.label1.Text = "Esta es una aplicación que monitorea el estado de la batería y notifica cuando no" +
     " esté en los niveles óptimos de funcionamiento.";
             // 
@@ -80,20 +82,39 @@
             // LbVersionTxt
             // 
             this.LbVersionTxt.AutoSize = true;
-            this.LbVersionTxt.Location = new System.Drawing.Point(181, 85);
+            this.LbVersionTxt.Location = new System.Drawing.Point(181, 113);
             this.LbVersionTxt.Name = "LbVersionTxt";
-            this.LbVersionTxt.Size = new System.Drawing.Size(56, 17);
-            this.LbVersionTxt.TabIndex = 9;
-            this.LbVersionTxt.Text = "Versión";
+            this.LbVersionTxt.Size = new System.Drawing.Size(60, 17);
+            this.LbVersionTxt.TabIndex = 4;
+            this.LbVersionTxt.Text = "Versión:";
             // 
             // LbVersion
             // 
             this.LbVersion.AutoSize = true;
-            this.LbVersion.Location = new System.Drawing.Point(184, 112);
+            this.LbVersion.Location = new System.Drawing.Point(243, 113);
             this.LbVersion.Name = "LbVersion";
             this.LbVersion.Size = new System.Drawing.Size(29, 17);
-            this.LbVersion.TabIndex = 10;
+            this.LbVersion.TabIndex = 5;
             this.LbVersion.Text = "V.1";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(178, 93);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(284, 17);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://danielrinconr.github.io/BatteryStatus/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Página Web";
             // 
             // FormAbout
             // 
@@ -103,7 +124,9 @@
             this.Controls.Add(this.LbVersion);
             this.Controls.Add(this.LbVersionTxt);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.lnklb_emailAuthor);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.lb_InfoAuthor);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -124,5 +147,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label LbVersionTxt;
         private System.Windows.Forms.Label LbVersion;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
     }
 }
